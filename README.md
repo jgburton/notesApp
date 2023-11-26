97e2e07a2851808fa8aa164d8f0e8a02a09127dc
Jamie Thomas Grantham-Burton <jgranthamburton@Jamies-MacBook-Air>
2023-11-26 12:45:34 +1100

    Updates

--- README.md
+++ README.md
@@ -44,3 +44,7 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## Tutorial 
+
+[Tutorial](https://www.freecodecamp.org/news/full-stack-project-tutorial-create-a-notes-app-using-react-and-node-js/#the-end-why-not-try-the-bonus-challenges)
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+  ]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const resetState = () => {
+    setTitle("");
+    setContent("");
+  };
+  const newNote: Note = {
+    id: notes.length + 1,
+    title: title,
+    content: content,
+  };
+  const handleAddNote = (event: React.FormEvent) => {
+    event.preventDefault();
+    setNotes([newNote, ...notes]);
+    resetState();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          required
+        ></input>
+        <textarea
+          placeholder="Content"
+          rows={10}
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button onClick={handleAddNote} type="submit">
+          Add Note
+        </button>
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+          <div key={note.id} className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
